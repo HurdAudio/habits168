9ff22d47c56dc805aa33fc80316f68c91d249042
Devin Hurd <hurdaudio@gmail.com>
2019-06-01 22:44:00 -0600

    initial landing styling work

--- app/public/js/landing/landing.component.js
+++ app/public/js/landing/landing.component.js
@@ -13,9 +13,21 @@
       const vm = this;
 
       vm.$onInit = onInit;
+      vm.monthSelect = '_JanuaryA';
+      vm.userLoggedIn = false;
+
+      function setFooterMessage() {
+        let today = new Date();
+        if (today.getFullYear() > 2019) {
+          vm.footerMessage = '©2019 - ' + today.getFullYear() + ' HurdAudio';
+        } else {
+          vm.footerMessage = '©2019 HurdAudio'
+        }
+      }
 
       function onInit() {
         console.log("Landing is lit");
+        setFooterMessage();
 
       }
 
