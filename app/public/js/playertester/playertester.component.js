a6d78087079bca758b8c68ac2811105b3b06e653
hurdaudio <hurdaudio@gmail.com>
2020-02-16 19:18:59 -0700

    player - stage 7 - january c

--- app/public/js/playertester/playertester.component.js
+++ app/public/js/playertester/playertester.component.js
@@ -13,7 +13,7 @@
       const vm = this;
 
       vm.$onInit = onInit;
-      vm.playerMonth = '_FebruaryB';
+      vm.playerMonth = '_FebruaryC';
       vm.playerFeeds = [
         {
           url: "https://feeds.buzzsprout.com/361115.rss",
