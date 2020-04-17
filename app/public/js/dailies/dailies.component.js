5723736f72bd1824e0ea9919b4f1a1cb992a03ca
hurdaudio <hurdaudio@gmail.com>
2020-04-17 17:35:09 -0600

    dailies - stage 5 - february a

--- app/public/js/dailies/dailies.component.js
+++ app/public/js/dailies/dailies.component.js
@@ -13,77 +13,9 @@
         const vm = this;
 
         vm.$onInit = onInit;
-        vm.dailiesMonth = '_JanuaryC';
+        vm.dailiesMonth = '_FebruaryA';
         vm.dailiesContainer = 'dailiesContainerActive' + vm.dailiesMonth;
         vm.navigateToHub = navigateToHub;
-        //        vm.dailySubs = [
-        //            {
-        //                uuid: 'b1c2eff2-81e8-4f19-a1ba-8fff123fb89a',
-        //                author: null,
-        //                description: 'A source for news on music that is challenging, interesting, different, progressive, introspective, or just plain weird',
-        //                link: 'https://avantmusicnews.com/',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/cropped-cropped-amnbanner1.jpg',
-        //                items: null,
-        //                rss: 'https://avantmusicnews.com/rss',
-        //                title: 'Avant Music News',
-        //                userRead: false
-        //            },
-        //            {
-        //                uuid: '9546ff34-ff07-4e31-9332-786ea544ec47',
-        //                author: null,
-        //                description: 'A blog about Major League Baseball',
-        //                link: 'https://www.baseballmusings.com/',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/788443.png',
-        //                items: null,
-        //                rss: 'http://feeds2.feedburner.com/Baseballmusingscom',
-        //                title: 'Baseball Musings',
-        //                userRead: false
-        //            },
-        //            {
-        //                uuid: '321d2897-f108-45c1-96ca-3aa70ec46590',
-        //                author: null,
-        //                description: 'The main page.',
-        //                link: 'http://www.dailykos.com/blogs/main',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/daily-kos-squarelogo-1517554340259.png',
-        //                items: null,
-        //                rss: 'https://feeds.dailykos.com',
-        //                title: 'Daily Kos',
-        //                userRead: false
-        //            },
-        //            {
-        //                uuid: '2c7be513-9083-439c-937f-42bbe95c0cb9',
-        //                author: null,
-        //                description: 'Links for the intellectually curious, ranked by readers.',
-        //                link: 'https://news.ycombinator.com/',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/hacker-news-2-569388.png',
-        //                items: null,
-        //                rss: 'https://news.ycombinator.com/rss',
-        //                title: 'Hacker News',
-        //                userRead: false
-        //            },
-        //            {
-        //                uuid: 'b081d79b-ff77-4586-83f3-e9eea5e22c9a',
-        //                author: null,
-        //                description: 'The news according to John Marshall',
-        //                link: 'https://talkingpointsmemo.com/',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/tpm-sq.svg',
-        //                items: null,
-        //                rss: 'https://talkingpointsmemo.com/feed/all',
-        //                title: 'Talking Points Memo',
-        //                userRead: false
-        //            },
-        //            {
-        //                uuid: '5eb65b5e-6896-4357-94af-e48fad7c64cc',
-        //                author: null,
-        //                description: 'Wonkette',
-        //                link: 'https://www.wonkette.com/',
-        //                image: 'https://habits168-hurdaudio.s3.amazonaws.com/feed_icons/wonkette.jpg',
-        //                items: null,
-        //                rss: 'https://www.wonkette.com/feeds/feed.rss',
-        //                title: 'Wonkette',
-        //                userRead: false
-        //            }
-        //          ];
         vm.availableSubs = [];
         vm.filterAvailables = filterAvailables;
         vm.removeSubscription = removeSubscription;
@@ -352,6 +284,15 @@
                     break;
                 case ('_JanuaryC'):
                     vm.logoPath = 'https://habits168-hurdaudio.s3.amazonaws.com/img/job-3506038_1280.png';
+                    break;
+                case ('_FebruaryA'):
+                    vm.logoPath = 'https://habits168-hurdaudio.s3.amazonaws.com/img/coffee_png_by_piccolapersempre_dbuithu-fullview.png';
+                    break;
+                case ('_FebruaryB'):
+                    vm.logoPath = 'https://habits168-hurdaudio.s3.amazonaws.com/img/digital_painting__liquid_sleep_by_ukulelemoon_d888syz-pre.jpg';
+                    break;
+                case ('_FebruaryC'):
+                    vm.logoPath = 'https://habits168-hurdaudio.s3.amazonaws.com/img/coffee_cup_by_lashonda1980_dazsks6-pre.png';
                     break;
                 default:
                     alert('UNSUPPORTED MONTH SELECT for LOGO');
