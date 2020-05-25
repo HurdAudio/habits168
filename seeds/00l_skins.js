fe84d094379d92ab8bfe94a1589cecfcf90d1015
hurdaudio <hurdaudio@gmail.com>
2020-05-24 20:13:43 -0600

    restored and updated player skins

--- seeds/00l_skins.js
+++ seeds/00l_skins.js
@@ -53,7 +53,7 @@ exports.seed = function (knex, Promise) {
                             "december": [31]
                         },
                         "player": {
-                            "available":  ['_JanuaryA', '_JanuaryB', '_JanuaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_MarchA', '_MarchB'],
+                            "available":  ['_JanuaryA', '_JanuaryB', '_JanuaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_MarchA', '_MarchB', '_MarchC'],
                             "january": [ 31, '_JanuaryB', '_JanuaryA', '_JanuaryB', '_JanuaryB', '_JanuaryA', '_JanuaryB', '_JanuaryC', '_JanuaryA', '_JanuaryA', '_JanuaryC', '_JanuaryA', '_JanuaryA', '_JanuaryB', '_JanuaryB', '_JanuaryB', '_JanuaryB', '_JanuaryA', '_JanuaryC', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryA', '_JanuaryB', '_JanuaryC', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryB', '_JanuaryC', '_JanuaryC', '_JanuaryA' ],
                             "february": [ 29, '_FebruaryC', '_FebruaryA', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_FebruaryC', '_FebruaryB', '_FebruaryC', '_FebruaryA', '_FebruaryA', '_FebruaryA', '_FebruaryC', '_FebruaryA', '_FebruaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryB', '_FebruaryC', '_FebruaryA', '_FebruaryB', '_FebruaryB', '_FebruaryC', '_FebruaryB' ],
                             "march": [31],
