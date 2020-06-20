3a09f5cbc2f7276488b78711ae85754ee28420aa
hurdaudio <hurdaudio@gmail.com>
2020-06-19 21:44:54 -0600

    user hub - stage 12 - class constructor

--- seeds/00l_skins.js
+++ seeds/00l_skins.js
@@ -86,7 +86,7 @@ exports.seed = function (knex, Promise) {
                             "available": ['_JanuaryA', '_JanuaryB', '_JanuaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_MarchA', '_MarchB', '_MarchC'],
                             "january": [ 31, '_JanuaryB', '_JanuaryA', '_JanuaryA', '_JanuaryA', '_JanuaryA', '_JanuaryB', '_JanuaryA', '_JanuaryB', '_JanuaryA', '_JanuaryB', '_JanuaryA', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryB', '_JanuaryB', '_JanuaryC', '_JanuaryB', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryB', '_JanuaryB', '_JanuaryB', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryC' ],
                             "february": [ 29, '_FebruaryA', '_FebruaryC', '_FebruaryB', '_FebruaryC', '_FebruaryA', '_FebruaryA', '_FebruaryC', '_FebruaryA', '_FebruaryB', '_FebruaryA', '_FebruaryB', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryA', '_FebruaryC', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryA', '_FebruaryC', '_FebruaryA', '_FebruaryC', '_FebruaryB', '_FebruaryB', '_FebruaryC' ],
-                            "march": [31],
+                            "march": [ 31, '_MarchB', '_MarchA', '_MarchA', '_MarchC', '_MarchA', '_MarchA', '_MarchB', '_MarchC', '_MarchA', '_MarchA', '_MarchB', '_MarchC', '_MarchB', '_MarchC', '_MarchC', '_MarchB', '_MarchB', '_MarchC', '_MarchA', '_MarchA', '_MarchB', '_MarchA', '_MarchA', '_MarchB', '_MarchB', '_MarchC', '_MarchC', '_MarchC', '_MarchC', '_MarchB', '_MarchA' ],
                             "april": [30],
                             "may": [31],
                             "june": [30],
