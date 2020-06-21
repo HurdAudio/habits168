ad4e11a65bdda182715fe9d0f6cd641c12168704
hurdaudio <hurdaudio@gmail.com>
2020-06-21 14:25:53 -0600

    tech blogs - stage 3 - january c

--- app/public/js/browse/browse.component.js
+++ app/public/js/browse/browse.component.js
@@ -13,7 +13,7 @@
         const vm = this;
 
         vm.$onInit = onInit;
-        vm.browseMonth = '_JanuaryB';
+        vm.browseMonth = '_JanuaryC';
         vm.navigateToHub = navigateToHub;
         vm.browseContainer = 'browseActive' + vm.browseMonth;
         vm.blogFeedTabState = 'browseTabActive' + vm.browseMonth;
