3a8761b4d299199a4775b5104f0c35a4ab542df6
hurdaudio <hurdaudio@gmail.com>
2020-09-13 10:13:57 -0600

    generic blogs - stage 9 - march a

--- seeds/00l_skins.js
+++ seeds/00l_skins.js
@@ -8,7 +8,7 @@ exports.seed = function (knex, Promise) {
                     uuid: '9fc176e3-91f8-4864-a996-01a101f88893',
                     skins: {
                         "browse": {
-                            "available": ['_JanuaryA', '_JanuaryB', '_JanuaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC'],
+                            "available": ['_JanuaryA', '_JanuaryB', '_JanuaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_MarchA'],
                             "january": [ 31, '_JanuaryB', '_JanuaryA', '_JanuaryA', '_JanuaryB', '_JanuaryC', '_JanuaryA', '_JanuaryC', '_JanuaryA', '_JanuaryA', '_JanuaryC', '_JanuaryB', '_JanuaryB', '_JanuaryC', '_JanuaryB', '_JanuaryA', '_JanuaryB', '_JanuaryA', '_JanuaryC', '_JanuaryA', '_JanuaryC', '_JanuaryC', '_JanuaryB', '_JanuaryB', '_JanuaryB', '_JanuaryC', '_JanuaryC', '_JanuaryC', '_JanuaryA', '_JanuaryB', '_JanuaryA', '_JanuaryA' ],
                             "february": [ 29, '_FebruaryB', '_FebruaryA', '_FebruaryA', '_FebruaryC', '_FebruaryA', '_FebruaryB', '_FebruaryC', '_FebruaryC', '_FebruaryC', '_FebruaryA', '_FebruaryA', '_FebruaryC', '_FebruaryC', '_FebruaryC', '_FebruaryA', '_FebruaryC', '_FebruaryC', '_FebruaryB', '_FebruaryA', '_FebruaryB', '_FebruaryB', '_FebruaryB', '_FebruaryA', '_FebruaryB', '_FebruaryB', '_FebruaryC', '_FebruaryB', '_FebruaryA', '_FebruaryB' ],
                             "march": [31],
