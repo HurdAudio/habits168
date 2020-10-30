c9f7db49d2590a74f6f046d53a27547ea3ffa6d6
hurdaudio <hurdaudio@gmail.com>
2020-10-29 18:35:13 -0600

    new user confirm email - stage 5 - february a

--- app/public/js/emailconfirm/emailconfirm.component.js
+++ app/public/js/emailconfirm/emailconfirm.component.js
@@ -13,7 +13,7 @@
         const vm = this;
 
         vm.$onInit = onInit;
-        vm.emailConfirmMonth = '_JanuaryC';
+        vm.emailConfirmMonth = '_FebruaryA';
         vm.emailConfirmContainerState = 'confirmEmail_Active' + vm.emailConfirmMonth;
         vm.accountValid = 'unchecked';
         vm.resubEmail = resubEmail;
