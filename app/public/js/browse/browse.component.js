d2db4458ce6d21a83759e1147cd88068bf907e80
hurdaudio <hurdaudio@gmail.com>
2020-10-31 18:53:16 -0600

    generic podcasts - stage 10 - march b

--- app/public/js/browse/browse.component.js
+++ app/public/js/browse/browse.component.js
@@ -930,8 +930,8 @@
             $http.get('/skins/browse')
                 .then(skinData => {
                     let skin = skinData.data;
-                    vm.browseMonth = skin.browse;
-//                    vm.browseMonth = '_FebruaryC';
+//                    vm.browseMonth = skin.browse;
+                    vm.browseMonth = '_MarchB';
                     vm.browseContainer = 'browseActive' + vm.browseMonth;
                     vm.blogFeedTabState = 'browseTabActive' + vm.browseMonth;
                     vm.podcastFeedTabState = 'browseTabInactive' + vm.browseMonth;
